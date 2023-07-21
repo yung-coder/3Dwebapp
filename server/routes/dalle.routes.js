456580050448de06e6b391365b6434ef7ae9f593
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-22 00:09:42 +0530

    3d app temp

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '',
 });
 
 const openai = new OpenAIApi(config);
